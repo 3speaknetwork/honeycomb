2a8d0c906fb11677daffff3c6153d113f327803a
disregardfiat <sjettingerjr@gmail.com>
2024-06-16 15:58:37 -0400

    ++

--- processing_routes/dex.js
+++ processing_routes/dex.js
@@ -1129,7 +1129,7 @@ exports.transfer = (json, pc) => {
               item &&
               (order.pair == "hbd" ||
                 (order.pair == "hive" &&
-                  (price <= stats.icoPrice / 1000 || !config.features.ico))) &&
+                  ((order.token != 'SPK' && price <= stats.icoPrice / 1000) || !config.features.ico))) &&
               (order.type == "MARKET" ||
                 (order.type == "LIMIT" && order.rate >= price))
             ) {

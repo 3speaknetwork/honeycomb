3fb6863b089fa42af4562f2eeee2b6bc63b7760a
disregardfiat <sjettingerjr@gmail.com>
2024-06-27 13:06:14 -0400

    ++

--- processing_routes/validators.js
+++ processing_routes/validators.js
@@ -93,7 +93,7 @@ var PoA = {
           for (var j = 0; j < storers; j++) {
             order.push(contracts[i].n[Base64.fromNumber(j)])
           }
-          order = new Set(order)
+          order = [...new Set(order)]
           var acc = []
           for (var acc in accepted) {
             accepted[acc].p = order.indexOf(acc)

31889f7cdc7f97fae264550dd49189c386559bbf
disregardfiat <sjettingerjr@gmail.com>
2024-06-28 15:32:55 -0400

    ++

--- processing_routes/validators.js
+++ processing_routes/validators.js
@@ -220,7 +220,7 @@ var PoA = {
     const gte = this.getPrand58(account, prand)
     const range = parseInt(((val[account] >= cutoff ? cutoff * 2 : val[account] || 1) / total) * (stats.total_files * parseInt(stats.vals_target * 10000) / 288) * 7427658739)
     const lte = Base58.fromNumber(Base58.toNumber(gte) + range)
-    console.log(range, gte, lte)
+    if(gte.length != lte.length)console.log(range, gte, lte, prand, account, val, stats)
     return [gte, lte]
   },
   getPrand58(account, prand) {
